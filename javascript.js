0cd8054c7fa9ac696b41a4d57d509fceaf699dca
jdfeng1 <justinfeng64@gmail.com>
2022-08-20 16:21:06 -0500

    Create functions getComputerChoice and playRound

---
+++ javascript.js
@@ -0,0 +1,44 @@
+let getComputerChoice = () => {
+    let randCounter = Math.floor(Math.random()*3) + 1;
+    return (randCounter === 1) ? 'rock' :
+    (randCounter === 2) ? 'paper' : 'scissors';
+}
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            return 'LOSE: ROCK < PAPER'
+        }
+        if (computerSelection == 'scissors') {
+            return 'WIN: ROCK > SCISSORS'
+        }
+        if (computerSelection == 'rock') {
+            return 'TIE: ROCK = ROCK'
+        }
+    }
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'paper') {
+            return 'TIE: PAPER = PAPER'
+        }
+        if (computerSelection == 'scissors') {
+            return 'LOSE: PAPER < SCISSORS'
+        }
+        if (computerSelection == 'rock') {
+            return 'WIN: PAPER > ROCK'
+        }
+    }
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'paper') {
+            return 'WIN: SCISSORS > PAPER'
+        }
+        if (computerSelection == 'scissors') {
+            return 'TIE: SCISSORS = SCISSORS'
+        }
+        if (computerSelection == 'rock') {
+            return 'LOSE: SCISSORS < ROCK'
+        }
+    } else {
+        return 'NAW'
+    }
+}
+console.log(playRound('scissors', getComputerChoice()));
